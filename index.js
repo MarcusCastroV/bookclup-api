4ac211ad8fc3d7f14c71cadf5cf7a89317d572fb
Marcus <mvcv2016@gmail.com>
2023-04-02 16:32:45 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import db from "./src/models"
+import UserController from "./src/controllers/user"
+const app = express()
+
+app.post("/", UserController.create)
+
+app.listen(3333, async () => {
+  try {
+    await db.sequelize.authenticate()
+    console.log("App running and db connected")
+  } catch (error) {
+    console.log('Error running app: ', error)
+  }
+})
