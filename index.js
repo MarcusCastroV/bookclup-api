e25cdb96f84c648209c1f4a3a7659f0ed9b4db2c
Marcus <mvcv2016@gmail.com>
2023-04-05 23:41:25 -0300

    feat: finish user register

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-import express from "express"
-import db from "./src/models"
-import UserController from "./src/controllers/user"
-const app = express()
+import express from "express";
+import db from "./src/models";
+import UserController from "./src/controllers/user";
+const app = express();
 
-app.post("/", UserController.create)
+app.use(express.json());
+
+app.post("/", UserController.create);
 
 app.listen(3333, async () => {
   try {
-    await db.sequelize.authenticate()
-    console.log("App running and db connected")
+    await db.sequelize.authenticate();
+    console.log("App running and db connected");
   } catch (error) {
-    console.log('Error running app: ', error)
+    console.log("Error running app: ", error);
   }
-})
+});
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,20 +1,47 @@
 import { User } from "../models";
+import * as Yup from "yup";
+import bcrypt from "bcrypt";
 
 class UserController {
   async create(req, res) {
-    const user = new User({
-      name: "Marcus",
-      email: "marcus@teste.com",
-      password: "teste123",
-      password_hash: "teste123",
-      reset_password_token: "teste",
-      reset_password_token_sent_at: new Date(),
-      avatar_url: "teste_url"
-    });
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string()
+          .required("Nome é obrigatório.")
+          .min(3, "Nome deve conter ao menos 3 caracteres."),
+        email: Yup.string()
+          .email("E-mail inválido")
+          .required("E-mail é obrigatório."),
+        password: Yup.string()
+          .required("Senha é obrigatório")
+          .min(6, "Senha deve conter ao menos 6 caracteres."),
+      });
 
-    await user.save();
+      const existedUser = await User.findOne({
+        where: { email: req.body.email },
+      });
+    
+      if(existedUser) {
+        return res.status(400).json({ error: 'Usuário já existe'})
+      }
 
-    return res.json({ user });
+      await schema.validate(req.body);
+
+      const hashPassword = await bcrypt.hash(req.body.password, 8);
+
+      const user = new User({
+        ...req.body,
+        password: "",
+        password_hash: hashPassword,
+      });
+
+      await user.save();
+
+      return res.json({ user });
+    } catch (error) {
+      console.log({ error });
+      return res.status(400).json({ error: error?.message });
+    }
   }
 }
 
