80a10c9b9c0099b0cd7de6a91606c88639f8e6c9
Marcus <mvcv2016@gmail.com>
2023-04-06 15:33:35 -0300

    feat: login implemented

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,57 @@
 import { User } from "../models";
 import * as Yup from "yup";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 class UserController {
+  async login(req, res) {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email("E-mail inválido")
+          .required("E-mail é obrigatório."),
+        password: Yup.string()
+          .required("Senha é obrigatório"),
+      });
+
+      await schema.validate(req.body);
+
+      const user = await User.findOne({ where: { email: req.body.email } });
+
+      if (!user) {
+        return res.status(401).json({ error: "E-mail ou senha não conferem." });
+      }
+
+      const checkPassword = await bcrypt.compare(
+        req.body.password,
+        user.password_hash
+      );
+
+      if (!checkPassword) {
+        return res.status(401).json({ error: "E-mail ou senha não conferem." });
+      }
+
+      const token = jwt.sign({ id: user.id }, process.env.JWT_HASH, {
+        expiresIn: "30d",
+      });
+
+      const { id, name, email, avatar_url, createdAt } = user;
+
+      return res.json({
+        user: {
+        id,
+        name,
+        email,
+        avatar_url,
+        createdAt,
+      },
+        token,
+      });
+    } catch (error) {
+      return res.status(400).json({ error: error?.message });
+    }
+  }
+
   async create(req, res) {
     try {
       const schema = Yup.object().shape({
@@ -17,15 +66,15 @@ class UserController {
           .min(6, "Senha deve conter ao menos 6 caracteres."),
       });
 
+      await schema.validate(req.body);
+
       const existedUser = await User.findOne({
         where: { email: req.body.email },
       });
-    
-      if(existedUser) {
-        return res.status(400).json({ error: 'Usuário já existe'})
-      }
 
-      await schema.validate(req.body);
+      if (existedUser) {
+        return res.status(400).json({ error: "Usuário já existe" });
+      }
 
       const hashPassword = await bcrypt.hash(req.body.password, 8);
 
