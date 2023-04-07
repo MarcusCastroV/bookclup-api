125f996f7b3b9c8feb7376ac237b07f8af9c05c1
Marcus <mvcv2016@gmail.com>
2023-04-07 07:43:41 -0300

    feat: auth middleware implemented

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import authMiddleware from '../middlewares/auth'
+import UserController from "../controllers/user";
+
+const routes = new Router();
+
+// -------- unauthenticated routes --------
+routes.post("/user", UserController.create);
+routes.post("/login", UserController.login);
+
+
+// -------- authenticated routes --------
+routes.use(authMiddleware)
+routes.get('/user', UserController.get)
+
+export default routes;
